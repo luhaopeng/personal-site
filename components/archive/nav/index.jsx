9397699ca62a2c82f23f78a89c68ecb33b3b3059
卢浩鹏 <13071103+luhaopeng@users.noreply.github.com>
2019-03-06 23:47:07 +0800

    styled components for nav

--- components/archive/nav/index.jsx
+++ components/archive/nav/index.jsx
@@ -1,23 +1,59 @@
 import React from 'react'
-import './index.less'
+import styled from 'styled-components'
+import { primary_color, transparent } from '../../../utils/theme'
+
+const Ul = styled.ul`
+    list-style-type: none;
+`
+
+const A = styled.a`
+    font-size: 12px;
+    border-radius: 3px;
+    color: ${props => (props.selected ? '#fff' : '#586069')};
+    cursor: pointer;
+    display: block;
+    overflow: hidden;
+    position: relative;
+    text-decoration: none;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    padding: 0.5rem 1rem;
+    margin-bottom: 0.5rem;
+    background-color: ${props =>
+        props.selected ? primary_color : transparent};
+    transition: all linear 0.15s;
+`
 
 class YearList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedKey: '2019'
+        }
+    }
+
+    handleClick = e => {
+        let { key } = e.target.dataset
+        this.setState({ selectedKey: key })
+    }
+
     render() {
         return (
-            <ul className='year-list'>
-                <li>
-                    <a className='list-item selected'>2019</a>
-                </li>
-                <li>
-                    <a className='list-item'>2018</a>
-                </li>
-                <li>
-                    <a className='list-item'>2017</a>
-                </li>
-                <li>
-                    <a className='list-item'>2016</a>
-                </li>
-            </ul>
+            <Ul>
+                {['2019', '2018', '2017', '2016'].map(val => {
+                    return (
+                        <li key={val}>
+                            <A
+                                data-key={val}
+                                selected={this.state.selectedKey === val}
+                                onClick={this.handleClick}
+                            >
+                                {val}
+                            </A>
+                        </li>
+                    )
+                })}
+            </Ul>
         )
     }
 }
---
+++ utils/theme.js
@@ -0,0 +1,9 @@
+// color
+export const primary_color = '#1890ff'
+export const success_color = '#52c41a'
+export const warning_color = '#faad14'
+export const error_color = '#f5222d'
+export const transparent = 'rgba(0, 0, 0, 0)'
+
+// text
+export const font_size_base = '14px'
